1dbc1ad91b6422e8e33604431032a7c0f71536fe
zerogpm <zerogpm@gmail.com>
2018-09-18 12:09:56 -0700

    add BEM

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,15 +15,15 @@
     <body>
         <header class="header">
             <div class="header__logo-box">
-                <img src="{{ asset('/img/logo-white.png') }}" alt="Logo" class="header__logo-box__logo">
+                <img src="{{ asset('/img/logo-white.png') }}" alt="Logo" class="header__logo">
             </div>
 
             <div class="header__text-box">
-                <h1 class="header__text-box__primary">
-                    <span class="header__primary__main">OutDoors</span>
-                    <span class="header__primary__sub">is where life happens</span>
+                <h1 class="header-primary">
+                    <span class="header-primary--main">OutDoors</span>
+                    <span class="header-primary--sub">is where life happens</span>
                 </h1>
-                <a href="#" class="btn btn-white btn-animated">Discover our tours</a>
+                <a href="#" class="btn btn--white btn--animated">Discover our tours</a>
             </div>
         </header>
     </body>
